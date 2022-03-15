63e469119483920960eea7850f524d4b12538812
Por Xiong <porxiong2020@outlook.com>
2022-03-15 15:23:55 -0700

    first commit

---
+++ 3_1.py
@@ -0,0 +1,46 @@
+
+
+"""
+Relational Operator
+<, <=, >, >=, != (not), == 
+Logical Operator 
+and (both or all), or (either needs to be met), not, in, not in
+"""
+
+# true_or_false = "b" > "a"; # True 
+# true_or_false = "z" < "!"; # False 
+# true_or_false = "z" < "!" and "b" > "a" # Fasle 
+# true_or_false = "z" < "!" or "b" > "a" # True
+
+#true_or_false = [3, 5] < [3, 7] # - Homework 
+#true_or_false = [3, 5] < [3, 5, 6]
+#true_or_false = [3, 5] < [3, 7, 2]
+# true_or_false = [7, "three", 5] < [7, "two", 2]
+
+# true_or_false = 3 in [3, 8 ]
+
+# c = "abc"
+# true_or_false = c < "good" + "d" # False, adding = concatnating
+
+# #true_or_false = not("z" < "!" or "b" > "a") #False
+
+# print("true_or_false", true_or_false)
+
+# list1 = [6, 4, -5, 3.5]
+
+# print(list1)
+# list1.sort()
+
+# print(list1)
+
+# list2 = ["ha", "hi", "B", "7"]
+
+# print(list2)
+# list2.sort()
+# print(list2)
+
+# Monarchs [0] = ("George", 5)
+# Monarchs [0,1] = 5
+# monarchs [-1,1] = 1
+# monarchs [1:2] = ("Elizabeth", 2)
+
